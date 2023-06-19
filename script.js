8591e613e2acf68cc22d64604ec1b0b65d1fd1c6
lalit nath <lalitgosai2002@gmail.com>
2023-06-19 20:58:20 +0530

    desktop site and theme completed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let html = document.getElementsByTagName("html");
 let body = document.getElementsByTagName("body");
 let mode_toggler = document.getElementById("btn_mode_switcher");
-mode_toggler.onclick=()=>{
+/*mode_toggler.onclick=()=>{
     document.getElementById("toggle_mode_light").classList.toggle('active');
     if(body[0].classList[0]=='mode-dark'){
         body[0].classList.replace('mode-dark','mode-light');
@@ -9,3 +9,9 @@ mode_toggler.onclick=()=>{
         body[0].classList.replace('mode-light','mode-dark');
     }
 };
+*/
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  body[0].classList.toggle("mode-light")
+  body[0].classList.toggle("mode-dark")
+})
