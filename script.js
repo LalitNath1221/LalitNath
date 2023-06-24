b50d4957e35d67a378970816c2122cf41021ffb7
lalit nath <lalitgosai2002@gmail.com>
2023-06-24 16:29:47 +0530

    nav modified

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 let html = document.getElementsByTagName("html");
 let body = document.getElementsByTagName("body");
 let mode_toggler = document.getElementById("btn_mode_switcher");
-/*mode_toggler.onclick=()=>{
-    document.getElementById("toggle_mode_light").classList.toggle('active');
-    if(body[0].classList[0]=='mode-dark'){
-        body[0].classList.replace('mode-dark','mode-light');
-    }else{
-        body[0].classList.replace('mode-light','mode-dark');
-    }
-};
-*/
 const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
   body[0].classList.toggle("mode-light")
